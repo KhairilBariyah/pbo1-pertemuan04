0483785b993851f9e41981206e4b5567e9a6453c
KhairilBariyah <yayahelzira@gmail.com>
2020-04-26 16:39:07 +0700

    Contoh Kasus Array 2

---
+++ src/contohkasus/array/Array1.java
@@ -0,0 +1,35 @@
+package contohkasus.array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        String[] lebaran;
+        lebaran = new String[11];
+
+        lebaran[0] = "Asalamualaikum Wr.Wb";
+        lebaran[1] = "Saya Khairil Bariyah";
+        lebaran[2] = "Mengucapkan";
+        lebaran[3] = "Selamat Menunaikan Ibadah Puasa";
+        lebaran[4] = "Bagi yang menjalankannya";
+        lebaran[5] = "Dan Semoga Pandemi Cepat Berlalu";
+        lebaran[6] = "Agar Bisa Melaksanakan Kuliah Tatap Muka";
+        lebaran[7] = "Aamiin";
+        lebaran[8] = "Karna Sedang memassuki Bulan Ramadhan";
+        lebaran[9] = "Maka Saya Minta Maaf lahir Dan Batin";
+        lebaran[10]= "Wassalamualaikum Wr. Wb";
+
+        System.out.println(lebaran[0]);
+        System.out.println(lebaran[1]);
+        System.out.println(lebaran[2]);
+        System.out.println(lebaran[3]);
+        System.out.println(lebaran[4]);
+        System.out.println(lebaran[5]);
+        System.out.println(lebaran[6]);
+        System.out.println(lebaran[7]);
+        System.out.println(lebaran[8]);
+        System.out.println(lebaran[9]);
+        System.out.println(lebaran[10]);
+
+
+    }
+}
+
